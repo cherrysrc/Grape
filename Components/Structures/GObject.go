3c46e496b9b33b4ee5c940d18e67051b058d1605
Jerrit Gläsker <50867655+cherrysrc@users.noreply.github.com>
2020-08-16 12:02:51 +0200

    Minor Typographic Change

--- Components/Structures/GObject.go
+++ Components/Structures/GObject.go
@@ -21,47 +21,47 @@ type GObject struct {
 //IObject interface implementation
 //--------------------
 
-var letterBytes string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 //Generates a pseudo random id for objects that dont have one
-func (obj *GObject) GenerateID(n int) {
+func (object *GObject) GenerateID(n int) {
 	b := make([]byte, n)
 	for i := range b {
 		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
 	}
-	obj.ID = string(b)
+	object.ID = string(b)
 }
 
 //Calculates the geometric center of a shape
-func (obj *GObject) CalculateCenter() {
+func (object *GObject) CalculateCenter() {
 	avgX := 0.0
 	avgY := 0.0
 
-	for i := range obj.Vertices {
-		avgX += obj.Vertices[i][0]
-		avgY += obj.Vertices[i][1]
+	for i := range object.Vertices {
+		avgX += object.Vertices[i][0]
+		avgY += object.Vertices[i][1]
 	}
 
-	vertexCount := float64(len(obj.Vertices))
+	vertexCount := float64(len(object.Vertices))
 	avgX /= vertexCount
 	avgY /= vertexCount
 
-	obj.GeometricCenter = []float64{
+	object.GeometricCenter = []float64{
 		avgX,
 		avgY,
 	}
 }
 
 //Sets a given GObjects position
-func (obj *GObject) Translate(targetP []float64) {
-	for i := range obj.Vertices {
+func (object *GObject) Translate(targetP []float64) {
+	for i := range object.Vertices {
 		offset := []float64{
-			obj.Vertices[i][0] - obj.GeometricCenter[0],
-			obj.Vertices[i][1] - obj.GeometricCenter[1],
+			object.Vertices[i][0] - object.GeometricCenter[0],
+			object.Vertices[i][1] - object.GeometricCenter[1],
 		}
 
-		obj.Vertices[i][0] = targetP[0] + offset[0]
-		obj.Vertices[i][1] = targetP[1] + offset[1]
+		object.Vertices[i][0] = targetP[0] + offset[0]
+		object.Vertices[i][1] = targetP[1] + offset[1]
 	}
-	obj.GeometricCenter = targetP
+	object.GeometricCenter = targetP
 }
